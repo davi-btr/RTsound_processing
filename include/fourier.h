74ea489ccce3c9bd1cdff68be6913db2906d708f
davi-btr <105121115+davi-btr@users.noreply.github.com>
2022-09-01 17:08:40 +0200

    Add files via upload

---
+++ include/fourier.h
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+#define PI 3.14159265358979
+
+float dft_sqr_arg(float, float *, unsigned int, float);
+
+int fft(float [], float [], int);
+
+int fft_inv(float [], float [], int);
+
+int fft_real(float [], float [], int);
